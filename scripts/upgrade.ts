3d3d361fb653cf0a912bd3d110f85aef5d72d4fa
mananjabrik <manna.jabrik@gmail.com>
2022-06-20 13:19:43 +0700

    setup for upgrade contract

---
+++ scripts/upgrade.ts
@@ -0,0 +1,40 @@
+import { writeFile, writeFileSync } from "fs";
+
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers, upgrades } from "hardhat";
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const WalletInvest = await ethers.getContractFactory("WalletInvest");
+  const walletDeploy = await upgrades.upgradeProxy(
+    "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
+    WalletInvest
+  );
+
+  await walletDeploy.deployed();
+
+  console.log("WalletInvest Deployed to :", walletDeploy.address);
+  const data = `export const WALLET_INVEST_ADDRESS = "${walletDeploy.address}";`;
+  writeFile("contractAddress.ts", data, function (err) {
+    if (err) throw err;
+    console.log("File saved!");
+  });
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
