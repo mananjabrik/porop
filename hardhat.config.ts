039d90fa597e13772ac8c772e16cf4ac3b0d6433
mananjabrik <manna.jabrik@gmail.com>
2022-06-21 00:21:18 +0700

    update deploying

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -44,7 +44,6 @@ const config: HardhatUserConfig = {
     testnet: {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
       chainId: 97,
-      gasPrice: 20000000000,
       accounts: { mnemonic: mnemonic },
     },
     mainnet: {
